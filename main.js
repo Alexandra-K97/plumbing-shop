19b2846b4ce4c2504072f68ba03cfce202b4e4b7
Alexandra-K97 <a.a.krivickay@gmail.com>
2024-11-04 14:44:57 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const menuButton = document.getElementById("menu-button")
+menuButton.addEventListener("click", function(){
+    console.log(1)
+})
+function click(a,b){
+  console.log(b,a)
+}
+click("hello",123)
+
